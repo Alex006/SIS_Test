82ea5cc0602dfba532966294a8e93753dba1b085
Cristian Padilla <Cristian.Padilla06@gmail.com>
2019-03-29 00:31:48 -0600

    SIS demo. Uploading sample project.

---
+++ src/main/java/com/sis/test/SisTestApplication.java
@@ -0,0 +1,13 @@
+package com.sis.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisTestApplication.class, args);
+	}
+
+}
